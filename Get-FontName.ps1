47c2d129b11c28878cfc960778bcc0c2ae3d43b4
Micky Balladelli <micky@balladelli.com>
2017-05-21 11:50:59 +0200

    Initial Version

---
+++ Get-FontName.ps1
@@ -0,0 +1,67 @@
+﻿function Get-FontName
+{
+<#
+.SYNOPSIS
+    Retrieves the font name from a TTF file.
+
+.DESCRIPTION
+    This cmdlet does not install a font. It retrieves the font name from the given file, or the provided path
+
+.PARAMETER Path
+    Specifies the path to the font files
+
+
+.PARAMETER Item
+    Specifies the file item object. This is provided by Get-Item or Get-ChildItem.
+
+.EXAMPLE 
+    Get-FontName -Path $myfontPath
+
+.EXAMPLE 
+    Get-ChildItem -Path *.ttf | Get-FontName
+
+.NOTES
+    Micky Balladelli
+#>
+    [CmdletBinding()]
+    PARAM(
+        [Parameter(
+            ParameterSetName='Path'
+        )]
+        [String]$Path,
+ 
+        [Parameter(
+            ValueFromPipeline = $true,
+            ParameterSetName='Item'
+        )]
+        [object[]]$Item
+
+    )
+
+    BEGIN
+    {
+        $ttfFiles = @()
+        $fontCollection = new-object System.Drawing.Text.PrivateFontCollection
+    }
+    
+    PROCESS
+    {
+        if ($Path -ne "")
+        {
+            $ttfFiles = Get-ChildItem $path
+        }
+        else
+        {
+            $ttfFiles += $Item
+        }
+
+    }
+
+    END
+    {
+        $ttfFiles | ForEach-Object {
+            $fontCollection.AddFontFile($_.fullname)
+            $fontCollection.Families[0].Name
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# Get-FontName
+
+Retrieves the font name from TTF file without install it.
+
+Get-FontName
+
+NAME
+    Get-FontName
+    
+SYNOPSIS
+    Retrieves the font name from a TTF file.
+    
+    
+SYNTAX
+    Get-FontName [-Path <String>] [<CommonParameters>]
+    
+    Get-FontName [-Item <Object[]>] [<CommonParameters>]
+    
+    
+DESCRIPTION
+    This cmdlet does not install a font. It retrieves the font name from the given file, or the provided path
+    
